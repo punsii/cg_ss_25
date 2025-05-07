36047eaab94477c48ffce13a61bdc02633d62460
lejosch01 <leon.marauder@gmail.com>
2025-05-07 20:22:45 +0200

    ugly solution but calculates all areas and assigns each city to the corresponding state correctly

--- src/bin/p02.rs
+++ src/bin/p02.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::fs::read_to_string;
+use std::iter::Map;
+use std::os::macos::raw::stat;
 
 // use core::f64::EPSILON; // => ~2.3E-16
 const EPSILON: f64 = 1E-12;
@@ -143,8 +145,8 @@ fn calculate_area_polygon(points: &Vec<Point>) -> f64 {
 
 fn calculate_area_triangle(point_0: &Point, point_1: &Point, point_2: &Point) -> f64 {
     let area: f64 = point_0.y * (point_2.x - point_1.x) / 2.0
-                  + point_1.y * (point_0.x - point_2.x) / 2.0
-                  + point_2.y * (point_1.x - point_0.x) / 2.0;
+        + point_1.y * (point_0.x - point_2.x) / 2.0
+        + point_2.y * (point_1.x - point_0.x) / 2.0;
     area
 }
 
@@ -158,9 +160,9 @@ fn main() {
         line = line.trim().to_string();
         if line.contains("id=") && !line.contains("svg") {
             current_id = line.split("id=").nth(1).unwrap()
-                             .split(" ").nth(0).unwrap()
-                             .replace("\"", "")
-                             .parse().unwrap();
+                .split(" ").nth(0).unwrap()
+                .replace("\"", "")
+                .parse().unwrap();
             if line.starts_with("id=") {
                 cities.insert(current_id.clone(), Point{ x: 0.0, y: 0.0 });
             } else {
@@ -253,20 +255,26 @@ fn main() {
     println!();
 
     for (city, point) in &cities {
-        // println!("city {}", city);
-        let mut state_of_city = "";
+        let mut state_of_city = String::new();
+        let mut possible_states: Vec<String> = Vec::new();
         for (state, vec) in &states {
-            // println!("state {}", state);
-            let mut in_polygons = 0;
             for (index, points) in vec.iter().enumerate() {
                 if point.is_in_polygon(points) {
-                    println!("{} -> {} ({})", city, state, index);
-                    in_polygons += 1;
+                    possible_states.push(state.clone());
                 }
             }
-            if in_polygons % 2 == 1 {
-                state_of_city = state;
-                break;
+        }
+        let mut state_occurrences: HashMap<String, usize> = HashMap::new();
+        for state in possible_states.clone() {
+            *state_occurrences.entry(state.clone()).or_insert(0) += 1;
+        }
+        if state_occurrences.len() == 1 {
+            state_of_city = possible_states[0].clone();
+        } else {
+            for (state, value) in state_occurrences {
+                if value == 1 {
+                    state_of_city = state.clone();
+                }
             }
         }
         println!("{} ({})", city, state_of_city);
