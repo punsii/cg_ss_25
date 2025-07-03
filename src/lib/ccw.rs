0f72b611e2db7c0ae60217372ff5547db0aacc9a
lejosch01 <leon.marauder@gmail.com>
2025-07-03 15:16:07 +0200

    added crossing point function

--- src/lib/ccw.rs
+++ src/lib/ccw.rs
@@ -83,8 +83,12 @@ impl CcwCombination {
     }
 }
 
+pub fn ccw_value(p: &Point, q: &Point, r: &Point) -> f64 {
+    p.x * q.y - p.y * q.x + q.x * r.y - q.y * r.x + p.y * r.x - p.x * r.y
+}
+
 pub fn ccw(p: &Point, q: &Point, r: &Point) -> i32 {
-    let ccw = p.x * q.y - p.y * q.x + q.x * r.y - q.y * r.x + p.y * r.x - p.x * r.y;
+    let ccw = ccw_value(p, q, r);
 
     if ccw < -EPSILON {
         -1
@@ -93,4 +97,22 @@ pub fn ccw(p: &Point, q: &Point, r: &Point) -> i32 {
     } else {
         1
     }
+}
+
+pub fn crossing_point(a1: &Point, a2: &Point, b1: &Point, b2: &Point) -> Point {
+    if a1.x == b1.x && a2.x == b2.x && a1.y == b1.y && b2.y == b2.y {
+        return a1.clone();
+    }
+    if a1.x == a2.x && a1.y == a2.y {
+        return a1.clone();
+    } else if b1.x == b2.x && b1.y == b2.y {
+        return b1.clone();
+    }
+    let d1 = ccw_value(b1, a1, b2).abs();
+    let d2 = ccw_value(b2, a2, b1).abs();
+    let b1_b2 = ((b2.x-b1.x)*(b2.x-b1.x) + (b2.y-b1.y)*(b2.y-b1.y)).sqrt();
+    let h1 = d1 / b1_b2;
+    let h2 = d2 / b1_b2;
+    let factor = h1 / (h1 + h2);
+    Point { x: a1.x + (a2.x-a1.x) * factor, y: a1.y + (a2.y-a1.y) * factor }
 }
